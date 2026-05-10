687e5beba34fc1a9afc4c91c9447c69a03699e73
we15i-creator <rtxg2093@gmail.com>
2026-05-10 20:33:59 +0545

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    id("com.android.application") version "8.5.0"
-    id("org.jetbrains.kotlin.android") version "2.0.21"
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
+
 android {
     namespace = "com.tubesheild"
     compileSdk = 34
-
     defaultConfig {
         applicationId = "com.tubesheild"
         minSdk = 24
@@ -13,25 +13,13 @@ android {
         versionCode = 1
         versionName = "0.1.0"
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
-        }
-    }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+    kotlinOptions { jvmTarget = "17" }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
 }
