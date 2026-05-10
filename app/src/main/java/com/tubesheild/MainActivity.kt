ef6b3e6c01e90bee03c45e34cbfa6660f8103b8e
we15i-creator <rtxg2093@gmail.com>
2026-05-10 21:45:29 +0545

    Update MainActivity.kt

--- app/src/main/java/com/tubesheild/MainActivity.kt
+++ app/src/main/java/com/tubesheild/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
             javaScriptEnabled = true
             domStorageEnabled = true
             databaseEnabled = true
+            mediaPlaybackRequiresUserGesture = false // Allows background play start
         }
 
         webView.webViewClient = object : WebViewClient() {
@@ -30,6 +31,7 @@ class MainActivity : AppCompatActivity() {
                 // Injecting scripts
                 view.evaluateJavascript(CLEANER_SCRIPT, null)
                 view.evaluateJavascript(FINGERPRINT_SCRIPT, null)
+                view.evaluateJavascript(BACKGROUND_PLAY_SCRIPT, null)
             }
 
             override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
@@ -40,21 +42,51 @@ class MainActivity : AppCompatActivity() {
         webView.loadUrl("https://m.youtube.com")
     }
 
-    fun clearSession() {
-        webView.evaluateJavascript(SESSION_CLEAR, null)
+    // IMPORTANT: Keep empty to prevent WebView from pausing when app is minimized
+    override fun onPause() {
+        // super.onPause() is omitted purposely to keep audio alive
+        super.onPause()
     }
 
     companion object {
         const val CLEANER_SCRIPT = """
             (function() {
                 var style = document.createElement('style');
-                style.innerHTML = '.ad-showing, ytd-ad-slot-renderer { display: none !important; }';
+                style.innerHTML = `
+                    .ad-showing, .ad-container, .ytp-ad-overlay-container, 
+                    ytd-ad-slot-renderer, #player-ads, .masthead-ad,
+                    .ytp-ad-progress-list { 
+                        display: none !important; 
+                    }
+                `;
                 document.head.appendChild(style);
                 
+                // Auto-skip mid-roll and pre-roll ads
                 setInterval(function() {
-                    var skipBtn = document.querySelector('.ytp-ad-skip-button');
+                    var skipBtn = document.querySelector('.ytp-ad-skip-button, .ytp-ad-skip-button-modern');
                     if (skipBtn) skipBtn.click();
+                    
+                    // Remove ad overlays if they appear
+                    var adOverlay = document.querySelector('.ytp-ad-overlay-close-button');
+                    if (adOverlay) adOverlay.click();
                 }, 500);
+            })();
+        """
+
+        const val BACKGROUND_PLAY_SCRIPT = """
+            (function() {
+                // Prevents YouTube from pausing when the tab/app is hidden
+                Object.defineProperty(document, 'hidden', { value: false, writable: false });
+                Object.defineProperty(document, 'visibilityState', { value: 'visible', writable: false });
+                document.dispatchEvent(new Event('visibilitychange'));
+                
+                // Keep the video element playing even if the page tries to pause it
+                setInterval(function() {
+                    var video = document.querySelector('video');
+                    if (video && video.paused && !video.ended) {
+                        video.play();
+                    }
+                }, 1000);
             })();
         """
 
