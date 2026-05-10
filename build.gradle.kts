acd8598c580c23f8d9556db1564b636f3d647690
we15i-creator <rtxg2093@gmail.com>
2026-05-10 18:12:45 +0545

    Update build.gradle.kts for plugin management
    Refactor build.gradle.kts to use plugin and dependency resolution management.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,18 @@
-plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TubeShield"
+include(":app")
