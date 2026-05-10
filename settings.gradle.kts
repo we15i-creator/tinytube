48b489b77ca6ca2fdf085f19756c33686ec369f5
we15i-creator <rtxg2093@gmail.com>
2026-05-10 21:26:19 +0545

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,9 +16,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "tinytube"
 include(":app")
-
-plugins {
-    // These versions are critical for Gradle 9.x compatibility
-    id("com.android.application") version "8.7.0"
-    id("org.jetbrains.kotlin.android") version "1.9.24"
-}
