e9e3157e6bdbb6a3ff2899605ad047300a092c22
we15i-creator <rtxg2093@gmail.com>
2026-05-10 21:36:53 +0545

    Update MainActivity.kt

--- app/src/main/java/com/tubesheild/MainActivity.kt
+++ app/src/main/java/com/tubesheild/MainActivity.kt
@@ -15,7 +15,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        // Initialize WebView
         webView = WebView(this)
         setContentView(webView)
 
@@ -26,20 +25,18 @@ class MainActivity : AppCompatActivity() {
         }
 
         webView.webViewClient = object : WebViewClient() {
-            override fun onPageFinished(view: WebView?, url: String?) {
-                super.onPageFinished(view: WebView?, url)
-                
-                // Inject the scripts defined in the companion object
-                webView.evaluateJavascript(CLEANER_SCRIPT, null)
-                webView.evaluateJavascript(FINGERPRINT_SCRIPT, null)
+            override fun onPageFinished(view: WebView, url: String) {
+                super.onPageFinished(view, url)
+                // Injecting scripts
+                view.evaluateJavascript(CLEANER_SCRIPT, null)
+                view.evaluateJavascript(FINGERPRINT_SCRIPT, null)
             }
 
-            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
-                return false // Stay inside the app
+            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+                return false 
             }
         }
 
-        // Load YouTube Mobile
         webView.loadUrl("https://m.youtube.com")
     }
 
@@ -47,20 +44,13 @@ class MainActivity : AppCompatActivity() {
         webView.evaluateJavascript(SESSION_CLEAR, null)
     }
 
-    // ALL CONSTANTS MUST BE IN THIS SINGLE BLOCK
     companion object {
         const val CLEANER_SCRIPT = """
             (function() {
                 var style = document.createElement('style');
-                style.innerHTML = `
-                    .ad-showing, .ad-container, .ytp-ad-overlay-container, 
-                    ytd-ad-slot-renderer, #player-ads, .masthead-ad { 
-                        display: none !important; 
-                    }
-                `;
+                style.innerHTML = '.ad-showing, ytd-ad-slot-renderer { display: none !important; }';
                 document.head.appendChild(style);
                 
-                // Logic to skip video ads automatically
                 setInterval(function() {
                     var skipBtn = document.querySelector('.ytp-ad-skip-button');
                     if (skipBtn) skipBtn.click();
@@ -70,9 +60,7 @@ class MainActivity : AppCompatActivity() {
 
         const val FINGERPRINT_SCRIPT = """
             (function() {
-                // Basic protection against browser fingerprinting
                 Object.defineProperty(navigator, 'webdriver', {get: () => false});
-                console.log('Shield Active: Fingerprint spoofing enabled');
             })();
         """
 
@@ -80,7 +68,6 @@ class MainActivity : AppCompatActivity() {
             (function() {
                 window.localStorage.clear();
                 window.sessionStorage.clear();
-                console.log('Session Cleared');
             })();
         """
     }
