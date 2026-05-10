e906d722196194b2be1d337d33157ca24df928c7
we15i-creator <rtxg2093@gmail.com>
2026-05-10 18:25:03 +0545

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.2.0"
-    id("org.jetbrains.kotlin.android") version "1.9.20"
+    id("com.android.application") version "8.5.0"
+    id("org.jetbrains.kotlin.android") version "2.0.21"
 }
 android {
     namespace = "com.tubesheild"
